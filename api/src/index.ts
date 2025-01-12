fa2e65df56251ace05d39827dbde9736d35ec230
prathapmohan27 <prathapmohan2704@gmail.com>
2025-01-12 23:00:06 +0530

    added the prisma orm

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,13 @@ const app: express.Application = express();
 
 const port: number = 3000;
 
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 // Server setup
 app.listen(port, () => {
   console.log(`Server running at
